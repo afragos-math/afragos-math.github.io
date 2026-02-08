05fffb0b45d0dc110cb1272761a7535d8205e2fe
afragos-math <60709592+afragos-math@users.noreply.github.com>
2026-02-08 11:23:15 +0200

    Polishing

---
+++ seminars/gradclubfiles/js/navigation.js
@@ -0,0 +1,8 @@
+function openNav() {
+      document.getElementById("mapmobilecontent").style.height = "100%";
+    }
+          
+function closeNav() {
+      document.getElementById("mapmobilecontent").style.height = "0%";
+    }
+
