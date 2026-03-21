b5b4550f32e7ea3f0decba843d1fb9278388de49
afragos-math <60709592+afragos-math@users.noreply.github.com>
2026-03-21 22:27:03 +0200

    upload

--- js/scrlback.js
+++ js/scrlback.js
@@ -1,3 +1,18 @@
+(function() {
+    let btnapp = document.getElementsByClassName("scrlbtn")[0];
+    window.onscroll = function() {scrlapp()};
+
+    function scrlapp() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btnapp.style.display = "block";
+        console.log(1);
+      } else {
+        btnapp.style.display = "none";
+        console.log(2);
+      }
+    }
+  })();
+
 function scrlback(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
