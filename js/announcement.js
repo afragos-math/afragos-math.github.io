433fbeecebf97384d381e6bdf3dafb674c798278
afragos-math <60709592+afragos-math@users.noreply.github.com>
2026-04-16 12:06:02 +0300

    upload

--- js/announcement.js
+++ js/announcement.js
@@ -4,6 +4,7 @@
       currtime = currtime.getHours()*100+currtime.getMinutes();
       var ongoingpgc26 = Array.from(document.getElementsByClassName("pgc26"));
       var ongoingfestum26 = Array.from(document.getElementsByClassName("festum26"));
+      var ongoinggradworkshop26 = Array.from(document.getElementsByClassName("gradworkshop26"));
       var ongoinggradclub4 = Array.from(document.getElementsByClassName("gradclub4"));
       var ongoingmanual = Array.from(document.getElementsByClassName("manual"));
       ongoingmanual.forEach(
@@ -19,7 +20,8 @@
             }
           );
         }
-      } else if(today >= 20260726 && today <= 20260801){
+      };
+      if(today >= 20260726 && today <= 20260801){
         if(currtime >= 830 && currtime < 2030){
           ongoingfestum26.forEach(
             box => {
@@ -27,7 +29,17 @@
             }
           );
         }
-      } else if(today <= 20260620){
+      };
+      if(today == 20260423){
+        if(currtime >= 1130 && currtime < 1500){
+          ongoinggradworkshop26.forEach(
+            box => {
+              box.style.display = 'inline-block';
+            }
+          );
+        }
+      };
+      if(today <= 20260620){
         ongoinggradclub4.forEach(
           box => {
             box.style.display = 'inline-block';
